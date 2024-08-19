16ca20b64586ee03a6fe13eb976711d440d7f58f
Toni-0809 <niki08y@gmail.com>
2024-08-19 17:55:34 +0300

    Добавьте файлы проекта.

---
+++ NoteBook3.App/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+    namespace NoteBook3_name.NoteBook3.App.Core
+    {
+        public class ObservableObject : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ NoteBook3.App/MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using NoteBook3_name.NoteBook3.App.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook3_name.NoteBook3.App
+{
+ 
+
+    public class MainViewModel : ObservableObject
+    {
+
+        private string _input = string.Empty;
+        public string Input
+        {
+            get => _input;
+            set
+            {
+                _input = value;
+                OnPropertyChanged("Input");
+            }
+        }
+
+        private ObservableCollection<Note> _NoteList2 = new ObservableCollection<Note>();
+        public ObservableCollection<Note> NoteList2 { get => _NoteList2; set { _NoteList2 = value; OnPropertyChanged("NoteList2"); } }
+
+        private NoteService2 NoteService2;
+
+        private Note _selectedNote;
+        public Note SelectedNote
+        {
+            get => _selectedNote;
+            set
+            {
+                __selectedNote = value;
+                OnPropertyChanged("SelectedNote");
+            }
+        }
+
+        public MainViewModel(NoteService2 service)
+        {
+            NoteService2 = service;
+            NoteList2 = new ObservableCollection<Note>(NoteService2.GetAll());
+        }
+
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      NoteService2.Create(
+                          new Note(Input)
+                          );
+                      NoteList2 = new ObservableCollection<Note>(NoteService2.GetAll());
+                  }));
+            }
+        }
+
+        private RelayCommand deleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                  (deleteCommand = new RelayCommand(obj =>
+                  {
+                      NoteService2.Delete(
+                          SelectedNote2.ItemId
+                          );
+                      NoteList2 = new ObservableCollection<Note>(NoteService2.GetAll());
+                  }));
+            }
+        }
+
+        private RelayCommand editCommand;
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return editCommand ??
+                  (editCommand = new RelayCommand(obj =>
+                  {
+                      SelectedNote2.Title = Input;
+                      NoteService2.Update(
+                          SelectedNote2
+                          );
+                      NoteList2 = new ObservableCollection<Note>(NoteService2.GetAll());
+                  }));
+            }
+        }
+
+
+    }
+}
